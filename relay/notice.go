098aa10a5b0a8007ccfa67cc2e98223c8714a1e0
fiatjaf <fiatjaf@alhur.es>
2021-01-13 23:45:50 -0300

    move relay and web-client out to other repositories.

--- relay/notice.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type Notice struct {
-	Kind    string `json:"kind"`
-	Message string `json:"message"`
-}
--- web-client/src/constants.js
+++
@@ -1,8 +0,0 @@
-export const KIND_METADATA = 0
-export const KIND_TEXTNOTE = 1
-export const KIND_RECOMMENDSERVER = 2
-export const KIND_CONTACTLIST = 3
-
-export const CONTEXT_REQUESTED = 'r'
-export const CONTEXT_PAST = 'p'
-export const CONTEXT_NOW = 'n'
